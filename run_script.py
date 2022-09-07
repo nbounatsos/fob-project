3b25f5be24da9c19b7f2d63fc42a345b22892057
Nick Bouonatsos <nikosbu@hotmail.com>
2022-09-08 00:35:21 +0200

    Updated the outpul file name

--- run_script.py
+++ run_script.py
@@ -4,7 +4,7 @@
 import os
 
 def main():
-	os.system("/home/banoffee/Documents/FoB/fob-project/skeleton_script_baseline_model.py -o /home/banoffee/Documents/FoB/results.tsv /home/banoffee/Documents/FoB/fob-project/HGVS_2020_small_VEP_baseline.tsv /home/banoffee/Documents/FoB/fob-project/BLOSUM62.txt")
+	os.system("/home/banoffee/Documents/FoB/fob-project/skeleton_script_baseline_model.py -o /home/banoffee/Documents/FoB/HGVS_2020_small_baseline_scores.tsv /home/banoffee/Documents/FoB/fob-project/HGVS_2020_small_VEP_baseline.tsv /home/banoffee/Documents/FoB/fob-project/BLOSUM62.txt")
 
 
 if __name__ == "__main__":
